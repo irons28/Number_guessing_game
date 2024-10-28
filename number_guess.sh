0204c706474d19db07b1500a9631a401199dc375
Ben Collen <ben.collen@me.com>
2024-10-28 23:25:44 +0000

    feat: Added not an integer script

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Number Guessing Game
-
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
@@ -13,7 +12,7 @@ then
   INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
   USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 else
-  USER_INFO=$($PSQL "SELECT games_played, best_game from users WHERE username='$USERNAME'")
+  USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
   IFS="|" read GAMES_PLAYED BEST_GAME <<< $USER_INFO
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
@@ -29,17 +28,17 @@ do
   ((GUESS_COUNT++))
   if [[ ! $GUESS =~ ^[0-9]+$ ]]
   then
-    echo "That is not an integer, Guess again:"
+    echo "That is not an integer, guess again:"
   elif [[ $GUESS -lt $SECRET_NUMBER ]]
   then
-    echo "Its's higher than that, try again:"
+    echo "It's higher than that, try again:"
   elif [[ $GUESS -gt $SECRET_NUMBER ]]
   then
     echo "It's lower than that, try again:"
   fi
 done
 
-echo "You guessed it in $GUESS_COUNT tries.  The secret number was $SECRET_NUMBER. Nice job!"
+echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
 
 UPDATE_USER_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE user_id = $USER_ID")
 if [[ -z $BEST_GAME || $GUESS_COUNT -lt $BEST_GAME ]]
@@ -47,4 +46,4 @@ then
   UPDATE_BEST_GAME=$($PSQL "UPDATE users SET best_game = $GUESS_COUNT WHERE user_id = $USER_ID")
 fi
 
-INSERT_GUESS_RESULT=$($PSQL "INSERT INTO guesses(user_id, guess, guess_number) VALUES($USER_ID, $GUESS, $GUESS_COUNT)")
+INSERT_GUESS_RESULT=$($PSQL "INSERT INTO guesses(user_id, guess, guess_number) VALUES($USER_ID, $SECRET_NUMBER, $GUESS_COUNT)")
