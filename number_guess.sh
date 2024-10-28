9a3ffc4e487ac545f525e410429422e763dbcc89
Ben Collen <ben.collen@me.com>
2024-10-28 23:21:22 +0000

    feat: Added secret number guessed script

--- number_guess.sh
+++ number_guess.sh
@@ -10,25 +10,26 @@ USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 if [[ -z $USER_ID ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
-  INSERT_USER_RESULT+$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
   USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 else
   USER_INFO=$($PSQL "SELECT games_played, best_game from users WHERE username='$USERNAME'")
   IFS="|" read GAMES_PLAYED BEST_GAME <<< $USER_INFO
-  ECHO "WELCOME BACK, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 echo "Guess the secret number between 1 and 1000:"
 GUESS_COUNT=0
 
+GUESS=-1
 while [[ $GUESS -ne $SECRET_NUMBER ]]
 do
   read GUESS
   ((GUESS_COUNT++))
   if [[ ! $GUESS =~ ^[0-9]+$ ]]
   then
-    echo "Please enter a valid number."
+    echo "That is not an integer, Guess again:"
   elif [[ $GUESS -lt $SECRET_NUMBER ]]
   then
     echo "Its's higher than that, try again:"
@@ -37,3 +38,13 @@ do
     echo "It's lower than that, try again:"
   fi
 done
+
+echo "You guessed it in $GUESS_COUNT tries.  The secret number was $SECRET_NUMBER. Nice job!"
+
+UPDATE_USER_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE user_id = $USER_ID")
+if [[ -z $BEST_GAME || $GUESS_COUNT -lt $BEST_GAME ]]
+then
+  UPDATE_BEST_GAME=$($PSQL "UPDATE users SET best_game = $GUESS_COUNT WHERE user_id = $USER_ID")
+fi
+
+INSERT_GUESS_RESULT=$($PSQL "INSERT INTO guesses(user_id, guess, guess_number) VALUES($USER_ID, $GUESS, $GUESS_COUNT)")
